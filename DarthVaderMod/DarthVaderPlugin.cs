92891325413bd64bbfe2f148a233cfef42546815
1TeaL <soxas.roxas@gmail.com>
2022-08-01 15:02:49 +1000

    readme and version updated

--- DarthVaderMod/DarthVaderPlugin.cs
+++ DarthVaderMod/DarthVaderPlugin.cs
@@ -37,7 +37,7 @@ namespace DarthVaderMod
         //   this shouldn't even have to be said
         public const string MODUID = "com.PopcornFactory.DarthVaderMod";
         public const string MODNAME = "DarthVaderMod";
-        public const string MODVERSION = "1.1.1";
+        public const string MODVERSION = "1.1.2";
 
         // a prefix for name tokens to prevent conflicts- please capitalize all name tokens for convention
         public const string DEVELOPER_PREFIX = "POPCORN";
